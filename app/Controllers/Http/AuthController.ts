f1be8aa291db9502e3dfbcbf8403d5a7d194a139
sarah abdeldaym <sarahabdeldaym@gmail.com>
2022-08-04 23:00:00 +0200

    Adding AuthController:Login Method

--- app/Controllers/Http/AuthController.ts
+++ app/Controllers/Http/AuthController.ts
@@ -1,7 +1,7 @@
 import type { HttpContextContract } from "@ioc:Adonis/Core/HttpContext";
 import { schema, rules } from "@ioc:Adonis/Core/Validator";
 import User from "App/Models/User";
-
+import Role from "App/Models/Role";
 
 export default class AuthController {
   public async register({ request, response }: HttpContextContract) {
@@ -21,5 +21,33 @@ export default class AuthController {
     response.status(200);
     return response.created({message : "user created successfully"});
   }
+
+  public async login({ request, response, auth }: HttpContextContract) {
+    const email = request.input("email");
+    const password = request.input("password");
+    try {
+      const token = await auth
+        .use("api")
+        .attempt(email, password, { expiresIn: "7days" });
+      await User.query()
+        .where("email", email)
+        .update({ remember_me_token: token.toJSON().token });
+
+      const user = await User.query().where("email", email);
+
+      const role = await Role.query().where("id", user[0].role_id);
+
+      return {
+        id: user[0].id,
+        name: user[0].name,
+        email: user[0].email,
+        role: role[0].name,
+        token: token.toJSON().token,
+      };
+    } catch {
+      return response.unauthorized("Invalid credentials");
+    }
+  }
+
 
 }
